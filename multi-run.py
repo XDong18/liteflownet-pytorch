5b37f25cf622db9ddcd664d2e41f9bccf42f5770
Xudong Liu <liuxd1215@gmail.com>
2020-07-28 22:49:29 +0800

    Update multi-run.py

--- multi-run.py
+++ multi-run.py
@@ -1,13 +1,18 @@
 import os
 import argparse
 
+def mkdir(path):
+	folder = os.path.exists(path)
+	if not folder:                  
+		os.makedirs(path)
+        
 def get_parser():
     parser = argparse.ArgumentParser(description='multi run')
     parser.add_argument('--list', type=str, help='list file path')
     parser.add_argument('--root', type=str, help='image root directory')
     parser.add_argument('--out', type=str, help='out directory')
     parser.add_argument('--model', type=str, help='model')
-    parser.add_argument('--show', type=int, default=10, help='show frequency')
+    parser.add_argument('--show', type=int, default=1, help='show frequency')
     return parser.parse_args()
 
 def main():
@@ -18,7 +23,8 @@ def main():
     for i, line in enumerate(pair_list):
         image_name_1 = line.strip(' \n').split(' ')[0]
         image_name_2 = line.strip(' \n').split(' ')[1]
-        out_name = os.path.join(args.out, image_name_1.split('/')[-1].split('.')[0] + '.flo')
+        mkdir(os.path.join(args.out, image_name_1.split('/')[0]))
+        out_name = os.path.join(args.out, image_name_1.split('.')[0] + '.flo')
         image_name_1 = os.path.join(args.root, image_name_1)
         image_name_2 = os.path.join(args.root, image_name_2)
         os.system('python run.py --model ' + args.model + ' --first '+ image_name_1 \
